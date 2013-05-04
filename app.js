6a33f440c093754d48ce841f2d60970e8c3af310
Evan Chiu <evan@evanchiu.com>
2013-05-04 18:53:44 -0400

    Connecting to the database to verify that words are real

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 // identif.io
 // app.js - the main node.js application
 
+// Imports
 var fs = require('fs'),
     path = require('path'),
     sio = require('socket.io'),
-    static = require('node-static');
+    static = require('node-static'),
+    levelup = require('levelup');
 
+// Configuration
+var database = './dictionary.level.db';
+
+// Database and list of words that have been used
+var db = levelup(database);
+var used = [];
+
+// File Server
 var app = require('http').createServer(handler);
 app.listen(8000);
 
@@ -15,16 +25,54 @@ function handler(req, res) {
     file.serve(req, res);
 }
 
+// Web Socket Server
 var io = sio.listen(app);
 
 io.sockets.on('connection', function (socket) {
 
     socket.on('letters', function (letters) {
-
-        socket.emit('result', letters);
-
-        setTimeout(function() {
-        	socket.emit('result', 'abcdefg');
-        }, 1000);
+        used = [];
+        combinations(letters, function(result) {
+            db.get(result, function(err) {
+                if (!err) {
+                    socket.emit('result', result);
+                }
+            })
+        })
     });
 });
+
+// function to run all combinations, then check the permutations of each
+function combinations(letters, callback) {
+    var fn = function(active, rest) {
+        if (!active && !rest)
+            return;
+        if (!rest) {
+            if (active.length > 1) {
+                permutations(active, callback);
+            }
+        } else {
+            fn(active + rest[0], rest.slice(1));
+            fn(active, rest.slice(1));
+        }
+    }
+    fn("", letters);
+}
+
+// function to check all permutations, making a callback when an unused
+// permutation is found
+function permutations(letters, callback) {
+    var fn = function(active, rest) {
+        if (!rest) {
+            if (used.indexOf(active) == -1) {
+                used.push(active);
+                callback(active);
+            }
+        } else {
+            for (var i = 0; i < rest.length; i++) {
+                fn(active + rest[i], rest.substr(0, i) + rest.substr(i+1));
+            }
+        }
+    }
+    fn("", letters);
+}
